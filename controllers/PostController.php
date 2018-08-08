1d66a8ec4def203ff6a3d3c8419e0065d125ec5f
batistapedro <batistapedro271188@gmail.com>
2018-08-07 21:55:38 -0400

    cambio en el behaviors

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -9,12 +9,31 @@ use yii\helpers\ArrayHelper;
 class PostController extends ActiveController
 {
     public $modelClass = 'micro\models\Post';
+    public $enableCsrfValidation=false;
 
-public function behaviors()
+    public function behaviors()
     {
-        // remove rateLimiter which requires an authenticated user to work
-        $behaviors = parent::behaviors();
-        unset($behaviors['rateLimiter']);
-        return $behaviors;
+      
+      return array_merge(
+        parent::behaviors(),[
+        'corsFilter'=>[
+          'class'=> \yii\filters\Cors::className(),
+          'cors'=>[
+              'Origin' => static::allowedDomains(),
+              'Access-Control-Request-Method'=>['GET','POST','PUT','DELETE'],
+              //'Access-Control-Request-Headers'=>['*'],
+              'Access-Control-Allow-Credentials'=>true,
+              'Access-Control-Max-Age'=>3600
+          ],
+        ],
+    ]);
+    }
+  
+    public static function allowedDomains()
+    {
+      return [
+        'http://localhost:4200',
+        'http://localhost/api/web/contactos'
+      ];
     }
 }
